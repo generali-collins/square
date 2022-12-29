79657c90b921c700c9051485c5ed6663cdf48251
generali.collins <104697851+generali-collins@users.noreply.github.com>
2022-12-29 12:02:03 +0000

    Add files via upload

---
+++ drawsquare.py
@@ -0,0 +1,9 @@
+def drawSquare(myTurtle, sideLength):
+    myTurtle.forward(sideLength)
+    myTurtle.right(90)
+    myTurtle.forward(sideLength)
+    myTurtle.right(90)
+    myTurtle.forward(sideLength)
+    myTurtle.right(90)
+    myTurtle.forward(sideLength)
+    myTurtle.right(90)
